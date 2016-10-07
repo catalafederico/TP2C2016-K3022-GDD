2da727c7192b39b845d8bc9808d2e65e6c41ecb1
Federico Catala Ortmann <catalafederico@gmail.com>
2016-10-07 13:01:25 -0300

    Resubo lo del login y la conexion de C# que no se habia hecho y cambio algunas cosas del script.

---
+++ ClinicaFrba/ClinicaFrba/BDComun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ClinicaFrba
+{
+    public class BDComun
+    {
+        public static SqlConnection obtenerConexion()
+        {
+            SqlConnection conexion = new SqlConnection("Data source=.\\SQLSERVER2012; Initial Catalog=GD2C2016;User Id=gd; Password=gd2016");
+            conexion.Open();
+            return conexion;
+
+        }
+    }
+}
---
+++ ClinicaFrba/ClinicaFrba/Inicio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace ClinicaFrba
+{
+    public class Inicio
+    {
+        private string usuario;
+        private string contraseña;
+
+
+        
+    }
+}

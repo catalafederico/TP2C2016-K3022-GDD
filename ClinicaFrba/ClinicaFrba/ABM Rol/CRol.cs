57674bd28f2ea7d75715e531b57a118627ba9af7
Guillermo Sequeira <Guillermo Sequeira>
2016-11-29 02:32:30 -0300

    saco db comun de algunas clases

--- ClinicaFrba/ClinicaFrba/ABM Rol/CRol.cs
+++ ClinicaFrba/ClinicaFrba/ABM Rol/CRol.cs
@@ -14,32 +14,5 @@ namespace ClinicaFrba.ABM_Rol
     class CRol
     {
 
-
-        public static DataTable obtenerFuncionalidades()
-        {
-
-            List<CRol> Lista = new List<CRol>();
-
-            using (SqlConnection conexion = BDComun.obtenerConexion())
-            {
-                
-                SqlCommand comando = new SqlCommand(string.Format("SELECT NOMBRE FROM [3FG].FUNCIONALIDADES"), conexion);
-
-                /*SqlDataReader reader = comando.ExecuteReader();*/
-
-                DataTable ds = new DataTable();
-
-                SqlDataAdapter dataAdapter = new SqlDataAdapter(comando);
-
-                dataAdapter.Fill(ds);
-
-                conexion.Close();
-
-                return ds;
-
-            }
-
-        }
-
     }
 }
